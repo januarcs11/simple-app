80af2e3afff695a375b4de57c953376a45da1b51
januarcs11 <januar.cs@gmail.com>
2026-04-30 08:13:09 +0000

    update github action 8

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return "Ini halaman Dengan flask github action "
+    return "Ini halaman Dengan flask github action testing "
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0", port=5001)
