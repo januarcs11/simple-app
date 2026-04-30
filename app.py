d42f0dd0d60cf69cfc93df7754d13e7b147664be
januarcs11 <73691864+januarcs11@users.noreply.github.com>
2026-04-30 18:29:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return "Ini halaman Dengan flask github action oke "
+    return "Ini halaman Dengan flask test jenkins"
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0", port=5001)
